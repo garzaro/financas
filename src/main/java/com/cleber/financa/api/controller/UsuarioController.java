b02a07b535bf8fa6226d92cdf386036a2f8db94b
garzaro <garzarocleber@gmail.com>
2020-05-18 23:41:00 -0300

    Implementado, salvar usuario

--- src/main/java/com/cleber/financa/api/controller/UsuarioController.java
+++ src/main/java/com/cleber/financa/api/controller/UsuarioController.java
@@ -1,12 +1,14 @@
 package com.cleber.financa.api.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cleber.financa.api.dto.UsuarioDTO;
+import com.cleber.financa.exceptions.RegraNegocioException;
 import com.cleber.financa.model.entity.Usuario;
 import com.cleber.financa.service.UsuarioService;
 
@@ -14,21 +16,36 @@ import com.cleber.financa.service.UsuarioService;
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
 	
-	UsuarioService usuarioService;
+	UsuarioService usuarioSerice;
 
+	//Contrutor para injeção de dependencia
 	public UsuarioController(UsuarioService usuarioService) {
-		this.usuarioService = usuarioService;
+		this.usuarioSerice = usuarioService;
 	}
-	@PostMapping
-	public ResponseEntity salvar(@RequestBody UsuarioDTO dto) { //dto transformado em entidade usuario
 		
-		Usuario usuario = Usuario.builder()
+//	ResponseEntity - Objeto que irá representar o corpo da resposta.
+//	dto transformado em entidade Usuario
+	public ResponseEntity salvar(UsuarioDTO dto) { 
+		
+		//dto transformado em entidade usuario
+		Usuario usuario = Usuario
+				.builder()
 				.nome(dto.getNome())
 				.email(dto.getEmail())
 				.senha(dto.getSenha())
 				.build();
+		try {
+			Usuario usuarioSalvo = usuarioSerice.salvarUsuario(usuario);
+			return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+		} catch (RegraNegocioException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+			
+		}
 		
 	}
+	
 		
+		
+		
+	
 }
-
