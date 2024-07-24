f1687c6d59d0d8822248e6943d9274b8578afe12
garzaro <clebergarzaro74@gmail.com>
2024-07-24 16:38:41 -0300

    metodo para validar email

--- src/test/java/com/cleber/financas/service/UsuarioServiceTest.java
+++ src/test/java/com/cleber/financas/service/UsuarioServiceTest.java
@@ -1,17 +1,30 @@
 package com.cleber.financas.service;
 
 import com.cleber.financas.model.repository.UsuarioRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
     @Autowired
     UsuarioRepository usuarioRepository;
     @Autowired
     UsuarioService usuarioService;
+
+    @Test(expected = Test.None.class)
+    public void deveValidarEmail(){
+        /*cenario*/
+        usuarioRepository.deleteAll();
+        /*ação*/
+        usuarioService.validarEmailNaBaseDedados("cleber@gmail.com");
+    }
+
 
 
 
