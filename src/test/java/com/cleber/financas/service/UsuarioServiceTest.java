f11762ff53f68428fce1bec13886491074706ba1
garzaro <clebergarzaro74@gmail.com>
2024-07-25 22:02:55 -0300

    metdo para lancar excessao com email ja cadastrado, adicionado

--- src/test/java/com/cleber/financas/service/UsuarioServiceTest.java
+++ src/test/java/com/cleber/financas/service/UsuarioServiceTest.java
@@ -1,5 +1,7 @@
 package com.cleber.financas.service;
 
+import com.cleber.financas.exception.RegraDeNegocioException;
+import com.cleber.financas.model.entity.Usuario;
 import com.cleber.financas.model.repository.UsuarioRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,4 +28,16 @@ public class UsuarioServiceTest {
         /*ação, sem verificação, só olha se existe o email*/
         usuarioService.validarEmailNaBaseDedados("cleber@gmail.com");
     }
+    @Test(expected = RegraDeNegocioException.class)
+    public void deveLancarErroAoValidarQuandoExistirEmaiLCadastrado(){
+        /*cenario*/
+        Usuario cadastrarEmail = Usuario.builder()
+                .nomeUsuario("garzaro")
+                .email("cleber@gmail.com")
+                .build();
+        usuarioRepository.save(cadastrarEmail);
+        /*ação*/
+        usuarioService.validarEmailNaBaseDedados("cleber@gmail.com");
+    }
+    
 }
