0b958914b4158c1f69b839eb3fee5ccd1a592b8a
garzaro <clebergarzaro74@gmail.com>
2024-07-25 22:48:49 -0300

    classe renomeada

--- src/main/java/com/cleber/financas/exception/ErroDeValidacao.java
+++
@@ -1,8 +0,0 @@
-package com.cleber.financas.exception;
-
-public class ErroDeValidacao extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-    public ErroDeValidacao(String messageErroValidacao) {
-        super(messageErroValidacao);
-    }
-}
