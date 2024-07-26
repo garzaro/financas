649e4b48aa04c577b10b596cb979667994f9a05e
garzaro <clebergarzaro74@gmail.com>
2024-07-25 23:04:10 -0300

    classe ára expor melhorias no teste

---
+++ src/test/java/com/cleber/financas/service/UsuarioServiceMelhoriasTest.java
@@ -0,0 +1,45 @@
+package com.cleber.financas.service;
+
+import com.cleber.financas.exception.RegraDeNegocioException;
+import com.cleber.financas.model.entity.Usuario;
+import com.cleber.financas.model.repository.UsuarioRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class UsuarioServiceMelhoriasTest {
+    
+    @Autowired
+    UsuarioService usuarioService;
+    
+    @Autowired
+    UsuarioRepository usuarioRepository;
+    
+    @Test(expected = Test.None.class)
+    public void deveValidarEmail(){
+        /*cenario*/
+        usuarioRepository.deleteAll();
+        /*ação, sem verificação, só olha se existe o email*/
+        usuarioService.validarEmailNaBaseDedados("cleber@gmail.com");
+    }
+    @Test(expected = RegraDeNegocioException.class)
+    public void deveLancarErroAoValidarQuandoExistirEmaiLCadastrado(){
+        /*cenario*/
+        Usuario cadastrarEmail = Usuario.builder()
+                .nomeUsuario("garzaro")
+                .email("cleber@gmail.com")
+                .build();
+        usuarioRepository.save(cadastrarEmail);
+        /*ação*/
+        usuarioService.validarEmailNaBaseDedados("cleber@gmail.com");
+    }
+    public void usuario(){
+    
+    }
+}
