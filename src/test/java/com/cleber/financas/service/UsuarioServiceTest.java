3fb3849d10b99ef12eedcbfbc6514c7616421e09
garzaro <clebergarzaro74@gmail.com>
2024-08-02 11:07:25 -0300

    ajustes

--- src/test/java/com/cleber/financas/service/UsuarioServiceTest.java
+++ src/test/java/com/cleber/financas/service/UsuarioServiceTest.java
@@ -3,8 +3,10 @@ package com.cleber.financas.service;
 import com.cleber.financas.exception.RegraDeNegocioException;
 import com.cleber.financas.model.entity.Usuario;
 import com.cleber.financas.model.repository.UsuarioRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -32,12 +34,30 @@ public class UsuarioServiceTest {
     public void deveLancarErroAoValidarQuandoExistirEmaiLCadastrado(){
         /*cenario*/
         Usuario cadastrarEmail = Usuario.builder()
-                .nomeUsuario("garzaro")
+                .nomeUsuario("garzaro74")
                 .email("cleber@gmail.com")
                 .build();
         usuarioRepository.save(cadastrarEmail);
         /*ação*/
         usuarioService.validarEmailNaBaseDedados("cleber@gmail.com");
+    }
+    @Test(expected = Test.None.class)
+    public void deveAutenticarUmUsuarioComSucesso(){
+        /*cenario*/
+        Usuario usuario = Usuario.builder()
+                .email("clebergarzaro@gmail.com")
+                .senha("senha123456")
+                .build();
+        /*ação*/
+        Usuario salvarUsuario = usuarioRepository.save(usuario);
+
+        // Verificar se o método autenticarUsuario retorna o usuário autenticado corretamente
+        Usuario usuarioAutenticado = usuarioService.autenticarUsuario(salvarUsuario.getEmail(), salvarUsuario.getSenha());
+
+        /*verificação*/
+        Assertions.assertThat(usuarioAutenticado).isNotNull();
+        Assertions.assertThat(usuarioAutenticado.getEmail()).isEqualTo(salvarUsuario.getEmail());
+        Assertions.assertThat(usuarioAutenticado.getSenha()).isEqualTo(salvarUsuario.getSenha());
     }
     
 }
