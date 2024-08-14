888a1fb8ac1fa558d37de2396da345a216cc9e70
garzaro <clebergarzaro74@gmail.com>
2024-08-14 15:18:42 -0300

    ajustes

--- src/test/java/com/cleber/financas/service/UsuarioServiceSpyTest.java
+++ src/test/java/com/cleber/financas/service/UsuarioServiceSpyTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class UsuarioServiceSpyTest {
+
     @SpyBean
     UsuarioServiceImpl usuarioServiceImpl;
     @MockBean
