39d5a44f0627f47f55d6322f9999a7adad99bde1
garzaro <clebergarzaro74@gmail.com>
2024-08-21 16:32:07 -0300

    adicionado builder()

--- src/main/java/com/cleber/financas/api/dto/UsuarioDTO.java
+++ src/main/java/com/cleber/financas/api/dto/UsuarioDTO.java
@@ -1,71 +1,103 @@
 package com.cleber.financas.api.dto;
 
+import java.time.LocalDate;
+
 /*Não usei @Builder, @Getter, @Setter
 * resolvi criar na mão o codigo*/
 public class UsuarioDTO {
-
+    private Long id;
     private String nomeCompleto;
     private String cadastroPessoaFisica;
     private String nomeUsuario;
     private String email;
     private String senha;
+    private LocalDate dataCadastro;
 
-    /*GETTERS AND SETTERS*/
-    public String getNomeCompleto() {
-        return nomeCompleto;
+    /*BUILDER*/
+    private UsuarioDTO(UsuarioBuilder builder) {
+        this.id = builder.id;
+        this.nomeCompleto = builder.nomeCompleto;
+        this.cadastroPessoaFisica = builder.cadastroPessoaFisica;
+        this.nomeUsuario = builder.nomeUsuario;
+        this.email = builder.email;
+        this.senha = builder.senha;
+        this.dataCadastro = builder.dataCadastro;
     }
 
-    public UsuarioDTO setNomeCompleto(String nomeCompleto) {
-        this.nomeCompleto = nomeCompleto;
-        return this;
+    public Long getId() {
+        return id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
     }
 
     public String getCadastroPessoaFisica() {
         return cadastroPessoaFisica;
     }
 
-    public UsuarioDTO setCadastroPessoaFisica(String cadastroPessoaFisica) {
-        this.cadastroPessoaFisica = cadastroPessoaFisica;
-        return this;
-    }
-
     public String getNomeUsuario() {
         return nomeUsuario;
-    }
-
-    public UsuarioDTO setNomeusuario(String nomeUsuario) {
-        this.nomeUsuario = nomeUsuario ;
-        return this;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public UsuarioDTO setEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
     public String getSenha() {
         return senha;
     }
 
-    public UsuarioDTO setSenha(String senha) {
-        this.senha = senha;
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public static class UsuarioBuilder{
+        private Long id;
+        private String nomeCompleto;
+        private String cadastroPessoaFisica;
+        private String nomeUsuario;
+        private String email;
+        private String senha;
+        private LocalDate dataCadastro;
+
+        public Long setId() {
+            this.id = id;
+            return id;
+        }
+
+        public UsuarioBuilder setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
         return this;
-    }
+        }
 
-    /*BUILDER*/
-    private UsuarioDTO(UsuarioDTO builder) {
-        this.nomeCompleto = builder.nomeCompleto;
-        this.cadastroPessoaFisica = builder.cadastroPessoaFisica;
-        this.nomeUsuario = builder.nomeUsuario;
-        this.email = builder.email;
-        this.senha = builder.senha;
-    }
+        public UsuarioBuilder setCadastroPessoaFisica(String cadastroPessoaFisica) {
+        this.cadastroPessoaFisica = cadastroPessoaFisica;
+        return this;
+        }
 
-    public UsuarioDTO build() {
-        return new UsuarioDTO(this);
+        public UsuarioBuilder setNomeUsuario(String nomeUsuario) {
+            this.nomeUsuario = nomeUsuario;
+            return this;
+        }
+
+        public UsuarioBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UsuarioBuilder setSenha(String senha){
+            this.senha = senha;
+            return this;
+        }
+
+        public UsuarioBuilder setDataCadastro(LocalDate dataCadastro){
+            this.dataCadastro = dataCadastro;
+            return this;
+        }
+
+        public UsuarioDTO build() {
+            return new UsuarioDTO(this);
+        }
     }
 }
