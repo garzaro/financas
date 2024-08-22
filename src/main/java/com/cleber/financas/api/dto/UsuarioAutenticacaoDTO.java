6afeff827b5624d71d6b0ac9730552fafdb954fa
garzaro <clebergarzaro74@gmail.com>
2024-08-22 19:34:57 -0300

    classe autenticação adicionada

---
+++ src/main/java/com/cleber/financas/api/dto/UsuarioAutenticacaoDTO.java
@@ -0,0 +1,14 @@
+package com.cleber.financas.api.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class UsuarioAutenticacaoDTO {
+	private String email;
+	private String senha;
+
+}
