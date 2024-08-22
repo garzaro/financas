c81abe071ac3a6fcf3c558593836eec9e0252fd7
garzaro <clebergarzaro74@gmail.com>
2024-08-22 19:32:22 -0300

    ajustes

--- src/main/java/com/cleber/financas/api/resource/UsuarioController.java
+++ src/main/java/com/cleber/financas/api/resource/UsuarioController.java
@@ -1,17 +1,15 @@
 package com.cleber.financas.api.resource;
 
-import com.cleber.financas.api.dto.UsuarioAutenticacaoDTO;
-import com.cleber.financas.exception.ErroDeAutenticacao;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cleber.financas.api.dto.UsuarioAutenticacaoDTO;
 import com.cleber.financas.api.dto.UsuarioCadastroDTO;
+import com.cleber.financas.exception.ErroDeAutenticacao;
 import com.cleber.financas.exception.RegraDeNegocioException;
 import com.cleber.financas.model.entity.Usuario;
 import com.cleber.financas.service.UsuarioService;
@@ -27,7 +25,7 @@ public class UsuarioController {
     }
 
     @PostMapping("/autenticar")
-    public ResponseEntity autenticarUsuario(@RequestBody @NotNull UsuarioAutenticacaoDTO dtoAuth) {
+    public ResponseEntity autenticarUsuario(@RequestBody UsuarioAutenticacaoDTO dtoAuth) {
         try {
             Usuario usuarioAutenticado = usuarioService.autenticarUsuario(dtoAuth.getEmail(), dtoAuth.getSenha());
             return ResponseEntity.ok(usuarioAutenticado);
@@ -41,7 +39,7 @@ public class UsuarioController {
     /*Salvar - Este método é um endpoint que recebe uma requisição HTTP POST*/
     /*ResponseEntity representa o corpo da resposta*/
     @PostMapping
-    public ResponseEntity salvar(@RequestBody @NotNull UsuarioCadastroDTO dto) {
+    public ResponseEntity salvar(@RequestBody UsuarioCadastroDTO dto) {
         Usuario usuario = Usuario.builder()
                 .nomeCompleto(dto.getNomeCompleto())
                 .cadastroPessoaFisica(dto.getCadastroPessoaFisica())
