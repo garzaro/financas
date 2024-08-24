496f95acd514829cc6b3ed9dfc88a6e66bbd9547
garzaro <clebergarzaro74@gmail.com>
2024-08-24 09:23:58 -0300

    uso do lombok par getters setters e builder

--- src/main/java/com/cleber/financas/api/dto/UsuarioAutenticacaoDTO.java
+++ src/main/java/com/cleber/financas/api/dto/UsuarioAutenticacaoDTO.java
@@ -3,24 +3,10 @@ package com.cleber.financas.api.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
+@Getter
+@Setter
+@Builder
 public class UsuarioAutenticacaoDTO {
     private String email;
     private String senha;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 }
