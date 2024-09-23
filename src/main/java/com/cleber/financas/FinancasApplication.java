d948b9d6b0e0eda60bdd1fe6dcdd9bd24cd468f5
garzaro <clebergarzaro74@gmail.com>
2024-09-23 10:16:03 -0300

    ajustes

--- src/main/java/com/cleber/financas/FinancasApplication.java
+++ src/main/java/com/cleber/financas/FinancasApplication.java
@@ -3,6 +3,8 @@ package com.cleber.financas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.Console;
+
 @SpringBootApplication
 public class FinancasApplication {
 
