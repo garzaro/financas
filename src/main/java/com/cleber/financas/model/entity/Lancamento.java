daad374f5f46e0f566b22349b08605c84d48f9e4
garzaro <clebergarzaro74@gmail.com>
2024-10-10 14:33:01 -0300

    data exception

--- src/main/java/com/cleber/financas/model/entity/Lancamento.java
+++ src/main/java/com/cleber/financas/model/entity/Lancamento.java
@@ -1,6 +1,6 @@
 package com.cleber.financas.model.entity;
 
-import com.cleber.financas.jackson.DeserializadorBigDecimalCustomizado;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import jakarta.persistence.*;
