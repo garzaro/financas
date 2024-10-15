e0b6500c5fba7a6f2011631be40f1967a6d648e3
garzaro <clebergarzaro74@gmail.com>
2024-10-14 21:33:55 -0300

    converter

---
+++ src/main/java/com/cleber/financas/api/converter/ConvertDtoToEntity.java
@@ -0,0 +1,49 @@
+package com.cleber.financas.api.converter;
+
+import com.cleber.financas.api.dto.LancamentoDTO;
+import com.cleber.financas.exception.RegraDeNegocioException;
+import com.cleber.financas.model.entity.Lancamento;
+import com.cleber.financas.model.entity.StatusLancamento;
+import com.cleber.financas.model.entity.TipoLancamento;
+import com.cleber.financas.model.entity.Usuario;
+import com.cleber.financas.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConvertDtoToEntity {
+    
+    @Autowired
+    private UsuarioService usuarioService;
+    
+    /* Um metodo para converter o dto em uma entidade de lancamento */
+    public Lancamento converterDtoParaEntidade(LancamentoDTO dto) {
+        Lancamento lancamento = new Lancamento();
+        lancamento.setId(dto.getId()); /* caso precise atualizar, ele vem preenchido com o id */
+        lancamento.setDescricao(dto.getDescricao());
+        lancamento.setAno(dto.getAno());
+        lancamento.setMes(dto.getMes());
+        lancamento.setDataCadastro(dto.getDataCadastro());
+        lancamento.setValor(dto.getValor());
+        /* lancamento.setDataCadastro(dto.getDataCadastro()); */
+        /* inicio usuario */
+        /* receber o id do usuario, conforme dto */
+        Usuario receberUsuario = usuarioService.obterUsuarioPorId(dto.getUsuario())
+                /*
+                 * buscarLancamento do usuario por id, ou lancar uma exception caso ele nao
+                 * exista
+                 */
+                .orElseThrow(() -> new RegraDeNegocioException(
+                        "Usuario não encontrado com o id " + "(" + dto.getUsuario() + ")"));
+        lancamento.setUsuario(receberUsuario);
+        /* fim usuario */
+        if (dto.getTipo() != null) {
+            lancamento.setTipoLancamento(TipoLancamento.valueOf(dto.getTipo()));
+        }
+        if (dto.getStatus() != null) {
+            lancamento.setStatusLancamento(StatusLancamento.valueOf(dto.getStatus()));
+        }
+        return lancamento;
+    }
+    
+}
