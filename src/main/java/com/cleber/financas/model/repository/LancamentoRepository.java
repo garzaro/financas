8c19a03e02792607c161c0569ccc1612bcc443f8
garzaro <clebergarzaro74@gmail.com>
2024-10-30 13:47:50 -0300

    teste lancamento

--- src/main/java/com/cleber/financas/model/repository/LancamentoRepository.java
+++ src/main/java/com/cleber/financas/model/repository/LancamentoRepository.java
@@ -5,9 +5,10 @@ import com.cleber.financas.model.entity.TipoLancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
-
+@Repository
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
     @Query(value = "select sum(l.valor) "
