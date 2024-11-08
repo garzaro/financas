d29c1a71b261adba0f5e18c74fb5401728b142a0
garzaro <clebergarzaro74@gmail.com>
2024-11-07 21:28:23 -0300

    finalizado teste rest api

--- src/test/java/com/cleber/financas/api/resource/UsuarioResourceRest.java
+++ src/test/java/com/cleber/financas/api/resource/UsuarioResourceRest.java
@@ -1,12 +1,13 @@
 package com.cleber.financas.api.resource;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.cleber.financas.api.dto.UsuarioAutenticacaoDTO;
+import com.cleber.financas.exception.ErroDeAutenticacao;
+import com.cleber.financas.exception.RegraDeNegocioException;
+import com.cleber.financas.model.entity.Usuario;
+import com.cleber.financas.service.LancamentoService;
+import com.cleber.financas.service.UsuarioService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
-/*@WebMvcTest(controllers = UsuarioController.class) - sobe o controller para teste
- * e define qual deles deve subir com o teste, serve para isolar das outras camadas*/
-/*@AutoConfigureMockMvc - autoconfigura o MockMvc para mockar rest api*/
-/*MockMvcRequestBuilders - objeto para criar requisição, verbos*/
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,64 +18,190 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.cleber.financas.api.dto.UsuarioAutenticacaoDTO;
-import com.cleber.financas.api.dto.UsuarioCadastroDTO;
-import com.cleber.financas.model.entity.Usuario;
-import com.cleber.financas.service.UsuarioService;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @WebMvcTest(controllers = UsuarioController.class)
 @ActiveProfiles("test")
 public class UsuarioResourceRest {
-
+    
     static final String API = "/api/usuarios";
     static final MediaType JSON = MediaType.APPLICATION_JSON; /*constante*/
-
+    
     @Autowired
     MockMvc mvc;
     @MockBean
     UsuarioService usuarioService;
-
+    @MockBean
+    LancamentoService lancamentoService;
+    
     @Test
     public void deveAutenticarUmUsuario() throws Exception {
         /*cenario*/
+        String email = "cleber@gmail.com";
+        String senha = "senha";
         UsuarioAutenticacaoDTO dto = UsuarioAutenticacaoDTO.builder() /*json a ser enviado*/
-                .email("cleber@gmail.com")
-                .senha("123456").build();
-
+                .email(email)
+                .senha(senha)
+                .build();
+        
         Usuario usuario = Usuario.builder() /*usuario autenticado*/
                 .id(1l)
-                .email("cleber@gmail.com")
-                .senha("123456").build();
+                .email(email)
+                .senha(senha)
+                .build();
         /*simulação de autenticação*/
-        Mockito.when(usuarioService.autenticarUsuario("cleber@gmail.com", "123456")).thenReturn(usuario);
+        Mockito.when(usuarioService.autenticarUsuario("cleber@gmail.com", "senha")).thenReturn(usuario);
         /*para transformar objeto de qualquer tipo em string json*/
         String json = new ObjectMapper().writeValueAsString(dto);
-
+        
         /*ação e verificação*/
         MockHttpServletRequestBuilder requisicao = MockMvcRequestBuilders
                 .post(API.concat("/autenticar"))
                 .accept(JSON) /*aceita receber objeto do tipo json*/
                 .contentType(JSON) /*enviar conteudo json*/
                 .content(json); /*passar o objeto json*/
-
+        
         mvc.perform(requisicao)
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("id").value(usuario.getId()))
-                .andExpect(MockMvcResultMatchers.jsonPath("nome").value(usuario.getNomeCompleto()))
+                .andExpect(MockMvcResultMatchers.jsonPath("nomeCompleto").value(usuario.getNomeCompleto()))
                 .andExpect(MockMvcResultMatchers.jsonPath("nomeUsuario").value(usuario.getNomeUsuario()))
-                .andExpect(MockMvcResultMatchers.jsonPath("emal").value(usuario.getEmail()))
-                .andExpect(MockMvcResultMatchers.jsonPath("senha").value(usuario.getSenha()))
-                .andExpect(MockMvcResultMatchers.jsonPath("cadastroPessoaFisica").value(usuario.getCadastroPessoaFisica()))
-        ;
-                /*considerar colocar jsonIgnore no dominio de usuario*/
-
+                .andExpect(MockMvcResultMatchers.jsonPath("email").value(usuario.getEmail()))
+                //.andExpect(MockMvcResultMatchers.jsonPath("senha").value(usuario.getSenha()))
+                .andExpect(MockMvcResultMatchers.jsonPath("cadastroPessoaFisica")
+                        .value(usuario.getCadastroPessoaFisica()));
     }
-
-
+    
+    @Test
+    public void deveRetornarBadRequestQuandoHouverErroDeAutenticao() throws Exception {
+        /*cenario*/
+        String email = "cleber@gmail.com";
+        String senha = "senha";
+        UsuarioAutenticacaoDTO dto = UsuarioAutenticacaoDTO.builder() /*json a ser enviado*/
+                .email(email)
+                .senha(senha)
+                .build();
+        /*simulação de autenticação*/
+        Mockito.when(usuarioService.autenticarUsuario(email, senha)).thenThrow(ErroDeAutenticacao.class);
+        /*para transformar objeto de qualquer tipo em string json*/
+        String json = new ObjectMapper().writeValueAsString(dto);
+        
+        /*ação e verificação*/
+        MockHttpServletRequestBuilder requisicao = MockMvcRequestBuilders
+                .post(API.concat("/autenticar"))
+                .accept(JSON) /*aceita receber objeto do tipo json*/
+                .contentType(JSON) /*enviar conteudo json*/
+                .content(json); /*passar o objeto json*/
+        
+        mvc.perform(requisicao)
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+    
+    @Test
+    public void deveSalvarUmUsuario() throws Exception {
+        /*cenario*/
+        String email = "cleber@gmail.com";
+        String senha = "senha";
+        UsuarioAutenticacaoDTO dto = UsuarioAutenticacaoDTO.builder() /*json a ser enviado*/
+                .email(email)
+                .senha(senha)
+                .build();
+        
+        Usuario usuario = Usuario.builder() /*usuario autenticado*/
+                .id(1l)
+                .email(email)
+                .senha(senha)
+                .build();
+        /*simulação de autenticação*/
+        Mockito.when(usuarioService.persistirUsuarioNabaseDeDados(Mockito.any(Usuario.class))).thenReturn(usuario);
+        /*para transformar objeto de qualquer tipo em string json*/
+        String json = new ObjectMapper().writeValueAsString(dto);
+        
+        /*ação e verificação*/
+        MockHttpServletRequestBuilder requisicao = MockMvcRequestBuilders
+                .post(API)
+                .accept(JSON) /*aceita receber objeto do tipo json*/
+                .contentType(JSON) /*enviar conteudo json*/
+                .content(json); /*passar o objeto json*/
+        
+        mvc.perform(requisicao)
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("id").value(usuario.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("nomeCompleto").value(usuario.getNomeCompleto()))
+                .andExpect(MockMvcResultMatchers.jsonPath("nomeUsuario").value(usuario.getNomeUsuario()))
+                .andExpect(MockMvcResultMatchers.jsonPath("email").value(usuario.getEmail()))
+                //.andExpect(MockMvcResultMatchers.jsonPath("senha").value(usuario.getSenha()))
+                .andExpect(MockMvcResultMatchers.jsonPath("cadastroPessoaFisica")
+                        .value(usuario.getCadastroPessoaFisica()));
+    }
+    
+    @Test
+    public void deveRetornarBadRequestAoTentarSalvarUmUsuarioInvalido() throws Exception {
+        /*cenario*/
+        String email = "cleber@gmail.com";
+        String senha = "senha";
+        UsuarioAutenticacaoDTO dto = UsuarioAutenticacaoDTO.builder() /*json a ser enviado*/
+                .email(email)
+                .senha(senha)
+                .build();
+        /*simulação de autenticação*/
+        Mockito.when(usuarioService.persistirUsuarioNabaseDeDados(Mockito.any(Usuario.class))).thenThrow(RegraDeNegocioException.class);
+        /*para transformar objeto de qualquer tipo em string json*/
+        String json = new ObjectMapper().writeValueAsString(dto);
+        
+        /*ação e verificação*/
+        MockHttpServletRequestBuilder requisicao = MockMvcRequestBuilders
+                .post(API)
+                .accept(JSON) /*aceita receber objeto do tipo json*/
+                .contentType(JSON) /*enviar conteudo json*/
+                .content(json); /*passar o objeto json*/
+        
+        mvc.perform(requisicao).andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+    
+    @Test
+    public void deveRetornarOSaldoDeUmUsuario() throws Exception {
+        /*cenario*/
+        String email = "cleber@gmail.com";
+        String senha = "senha";
+        Long id = 1l;
+        
+        BigDecimal saldo = BigDecimal.valueOf(1500);
+        Usuario usuario = Usuario.builder().id(id).email(email).senha(senha).build();
+        /*simulação de autenticação*/
+        Mockito.when(usuarioService.obterUsuarioPorId(id)).thenReturn(Optional.of(usuario));
+        Mockito.when(lancamentoService.obterSaldoPorUsuario(id)).thenReturn(saldo);
+        /*ação e verificação*/
+        MockHttpServletRequestBuilder requisicao = MockMvcRequestBuilders
+                .get(API.concat("/1/saldo"))
+                .accept(JSON) /*aceita receber objeto do tipo json*/
+                .contentType(JSON); /*enviar conteudo json*/
+        
+        mvc.perform(requisicao)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("1500"));
+    }
+    
+    @Test
+    public void deveResourceNaoEncontradoNaoExistirUsuarioParaObterSaldo() throws Exception {
+        /*cenario*/
+        Long id = 1l;
+        /*simulação de autenticação*/
+        Mockito.when(usuarioService.obterUsuarioPorId(id)).thenReturn(Optional.empty());
+        /*ação e verificação*/
+        MockHttpServletRequestBuilder requisicao = MockMvcRequestBuilders
+                .get(API.concat("/1/saldo"))
+                .accept(JSON) /*aceita receber objeto do tipo json*/
+                .contentType(JSON); /*enviar conteudo json*/
+        
+        mvc.perform(requisicao)
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
 }
