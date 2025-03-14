e897647205422636ad5e7b14a5846eb580513986
garzaro <clebergarzaro74@gmail.com>
2025-03-14 15:31:00 -0300

    permissao de acesso - CORS

--- src/main/java/com/cleber/financas/FinancasApplication.java
+++ src/main/java/com/cleber/financas/FinancasApplication.java
@@ -2,10 +2,22 @@ package com.cleber.financas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/*@EnableWebMvc - para liberar o acesso  -CORS
+* allowedOrigins - (url) - urls permitidas para acessar um site */
 @SpringBootApplication
-public class FinancasApplication {
-
+@EnableWebMvc
+public class FinancasApplication implements WebMvcConfigurer {
+	/*Adicionando mapeamento de cors*/
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		WebMvcConfigurer.super.addCorsMappings(registry);
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 
 		SpringApplication.run(FinancasApplication.class, args);
--- src/main/java/com/cleber/financas/model/entity/Usuario.java
+++ src/main/java/com/cleber/financas/model/entity/Usuario.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
@@ -13,6 +14,7 @@ import java.util.Objects;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 @Entity
 @Table(name= "usuario", schema = "financeiro")
 public class Usuario {
@@ -40,101 +42,6 @@ public class Usuario {
     @Convert(converter = Jsr310JpaConverters.LocalDateConverter.class)
     @Column(name = "data_cadastro")
     private LocalDate dataCadastro;
-
-    
-    
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNomeCompleto() {
-		return nomeCompleto;
-	}
-
-	public void setNomeCompleto(String nomeCompleto) {
-		this.nomeCompleto = nomeCompleto;
-	}
-
-	public String getCadastroPessoaFisica() {
-		return cadastroPessoaFisica;
-	}
-
-	public void setCadastroPessoaFisica(String cadastroPessoaFisica) {
-		this.cadastroPessoaFisica = cadastroPessoaFisica;
-	}
-
-	public String getNomeUsuario() {
-		return nomeUsuario;
-	}
-
-	public void setNomeUsuario(String nomeUsuario) {
-		this.nomeUsuario = nomeUsuario;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	public LocalDate getDataCadastro() {
-		return dataCadastro;
-	}
-
-	public void setDataCadastro(LocalDate dataCadastro) {
-		this.dataCadastro = dataCadastro;
-	}
-	
-	public static class UsuarioBuilder{
-		private String nomeCompleto;
-        private String cadastroPessoaFisica;
-        private String nomeUsuario;
-        private String email;
-        private String senha;
-        private LocalDate dataCadastro;
-	}
-            
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(cadastroPessoaFisica, dataCadastro, email, id, nomeCompleto, nomeUsuario, senha);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Usuario other = (Usuario) obj;
-		return Objects.equals(cadastroPessoaFisica, other.cadastroPessoaFisica)
-				&& Objects.equals(dataCadastro, other.dataCadastro) && Objects.equals(email, other.email)
-				&& Objects.equals(id, other.id) && Objects.equals(nomeCompleto, other.nomeCompleto)
-				&& Objects.equals(nomeUsuario, other.nomeUsuario) && Objects.equals(senha, other.senha);
-	}
-
-	@Override
-	public String toString() {
-		return "Usuario [id=" + id + ", nomeCompleto=" + nomeCompleto + ", cadastroPessoaFisica=" + cadastroPessoaFisica
-				+ ", nomeUsuario=" + nomeUsuario + ", email=" + email + ", senha=" + senha + ", dataCadastro="
-				+ dataCadastro + "]";
-	}
 
     /*GETTERS AND SETTERS*/
     
