ff216ab8637bc16e2c8d0e0ad32df281f4322ea2
garzaro <clebergarzaro74@gmail.com>
2025-04-28 11:43:07 -0300

    cors

--- src/main/java/com/cleber/financas/config/CorsConfig.java
+++ src/main/java/com/cleber/financas/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Configuration
 public class CorsConfig  implements WebMvcConfigurer{
-	/*Habilitando de cors -*/
+	/*Habilitando o cors -*/
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/api/")
