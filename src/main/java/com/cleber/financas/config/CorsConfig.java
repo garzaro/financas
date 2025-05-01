fbf3145a1e25019c26cff020be7803c44bf4c3a7
garzaro <clebergarzaro74@gmail.com>
2025-05-01 16:34:17 -0300

    cors /usuarios

--- src/main/java/com/cleber/financas/config/CorsConfig.java
+++ src/main/java/com/cleber/financas/config/CorsConfig.java
@@ -7,15 +7,35 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
 @Configuration
-public class CorsConfig  implements WebMvcConfigurer{
-	/*Habilitando de cors -*/
+public class CorsConfig  implements WebMvcConfigurer {
+	/*Habilitando cors -*/
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/api/usuarios/")
+		registry.addMapping("/api/")
 				.allowedOrigins("http://localhost:3000")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-				.allowedHeaders("Content-Type", "application/json");
+				.allowedHeaders("*")
+				//.allowedHeaders("Content-Type", "application/json", "Authentication")
+				.allowCredentials(false)
+				.maxAge(-1);
 	}
+}
+
+/*@EnableWebMvc
+@Configuration
+public class CorsConfig  implements WebMvcConfigurer{
+	/*Habilitando cors -
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/")
+				.allowedOrigins("http://localhost:3000")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedHeaders("*")
+				//.allowedHeaders("Content-Type", "application/json", "Authentication")
+				.allowCredentials(false)
+				.maxAge(-1);
+	}
+*/
 
 /*para permitir somente requisições vindas de um dominio especifico,
 e permitir somente metodos GET e POST, o codigo seria assim:
@@ -28,4 +48,4 @@ public void addCorsMappings(CorsRegistry registry) {
 }
 */
 
-}
+
