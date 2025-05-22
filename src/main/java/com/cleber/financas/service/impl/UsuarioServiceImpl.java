c5564135ae8d93248dec5d7a976011a3c8604073
garzaro <clebergarzaro74@gmail.com>
2025-05-22 12:21:09 -0300

    ajustes

--- src/main/java/com/cleber/financas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/cleber/financas/service/impl/UsuarioServiceImpl.java
@@ -1,38 +1,52 @@
 package com.cleber.financas.service.impl;
 
+import com.cleber.financas.config.PasswordEncoderConfig;
 import com.cleber.financas.exception.ErroDeAutenticacao;
 import com.cleber.financas.exception.RegraDeNegocioException;
 import com.cleber.financas.model.entity.Usuario;
 import com.cleber.financas.model.repository.UsuarioRepository;
 import com.cleber.financas.service.UsuarioService;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import jakarta.transaction.Transactional;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.beans.Encoder;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
+/* caso tenhamos que usa o bcrypt private PasswordEncoder passwordEncoder;*/
 //@NoArgsConstructor
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
-    
-    private UsuarioRepository usuarioRepository;
-    
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+    @Autowired
+    PasswordEncoderConfig passwordEncoder;
+
     public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
         super();
         this.usuarioRepository = usuarioRepository;
     }
-    /*lista de email permitidos*/
-    private static final List<String> dominiosEmaislPermitidos = List.of(
-            "gmail.com", "yahoo.com", "hotmail.com", "outlook.com", "edu.br", "org", "gov.br"
+    /* lista de emails permitidos */
+    private static final List<String> dominiosEmailPermitidos = List.of(
+            "gmail.com", "outlook.com", "edu.br", "gov.br"
     );
-    /*login: validação e autenticação*/
+
+    /*login, validação e autenticação*/
     @Override
     public Usuario autenticar(String email, String senha) {
         /*login, validando login*/
         Optional<Usuario> usuario = usuarioRepository.findByEmail(email);
         /*verificar a existencia de usuario na base de dados*/
         if (!usuario.isPresent()) {
-            throw new ErroDeAutenticacao("Verifique seu usuário e tente novamente.");
+            throw new ErroDeAutenticacao("Verifique seu email e tente novamente.");
         }
         if (!usuario.get().getSenha().equals(senha)) {
             throw new ErroDeAutenticacao("Senha incorreta. Tente novamente ou clique em \"Esqueceu a senha?\" para escolher outra.");
@@ -44,47 +58,103 @@ public class UsuarioServiceImpl implements UsuarioService {
     @Transactional
     public Usuario salvarUsuario(Usuario usuario) {
         /*deve validar o email e o cpf, verificar se existe*/
-        validacao(usuario.getEmail(), usuario.getCpf());
-        /*se nao existir email, salva a instancia*/
+        validarUsuario(usuario);
+        /*hash da senha antes de salvar a instancia*/
+        String hashSenha = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(hashSenha);
+        /*se nao existir email e nem cpf, salva a instancia com o hash da senha*/
+        Usuario usuarioSalvo = usuarioRepository.save(usuario);
+        /*remover a senha do objeto retornado para evitar vazamento*/
+        usuarioSalvo.setSenha(null);
+        return usuarioSalvo;
+    }
+
+    @Override
+    public Usuario atualizarUsuario(Usuario usuario) {
+        Objects.requireNonNull(usuario.getId());
+        validarUsuario(usuario);
         return usuarioRepository.save(usuario);
-    }  
+    }
 
     @Override
     public Optional<Usuario> obterUsuarioPorId(Long id) {
         return usuarioRepository.findById(id);
     }
 
-	@Override
-	public Optional<Usuario> obterUsuarioPorCpf(String cpf) {
-		return usuarioRepository.findByCpf(cpf);
-	}
-    
+    @Override
+    public Optional<Usuario> obterUsuarioPorCpf(String cpf) {
+        return  usuarioRepository.findByCpf(cpf);
+    }
+
+    /*VALIDAÇÃO*/
+    @Override
+    public void validarUsuario(Usuario usuario) {
+        /* preencher campos */
+        if (usuario.getNome() == null || usuario.getNome().trim().equals("")) {
+            throw new RegraDeNegocioException("O nome completo é obrigatório.");
+        }
+        if (usuario.getUsuario() == null || usuario.getUsuario().trim().equals("")) {
+            throw new RegraDeNegocioException("O nome de usuário é obrigatório.");
+        }
+        if (usuario.getEmail() == null || usuario.getEmail().trim().equals("")) {
+            throw new RegraDeNegocioException("O email é obrigatório.");
+        }
+        /*validação manual
+        if (!Pattern.matches("^[\\w-\\.]+@[\\w-\\.]+\\.[a-z]{2,}$", usuario.getEmail())) {
+            throw new ErroValidacaoException("O email deve seguir o padrao email@seudominio.com (br).");
+        }*/
+        if (usuario.getEmail() == null || usuario.getEmail().trim().equals("")) {
+            throw new RegraDeNegocioException("O email deve seguir o padrao email@seudominio.com (br).");
+        }
+
+        String emailPermitido = usuario.getEmail().substring(usuario.getEmail().lastIndexOf("@") + 1);
+        if (!dominiosEmailPermitidos.contains(emailPermitido)) { /*garante que o dominio extraido esteja na lista*/
+            throw new RegraDeNegocioException("O email para cadastro deve constar da lista a seguir: " + dominiosEmailPermitidos);
+        }
+
+        if (usuario.getCpf() == null || usuario.getCpf().trim().equals("")) {
+            throw new RegraDeNegocioException("O cpf é obrigatório.");
+        }
+        /*validação manual*/
+        if (!Pattern.matches("^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$", usuario.getCpf())) {
+            throw new RegraDeNegocioException("O CPF deve seguir o padrao 000.000.000-00");
+        }
+
+        if (usuario.getSenha() == null || usuario.getSenha().trim().equals("")) {
+            throw new RegraDeNegocioException("informe a senha.");
+        }
+        /*garante a nao duplicidade ao criar ou atualizar*/
+        validarDuplicidadeEmailCpf(usuario.getEmail(), usuario.getCpf());
+    }
+
+    public void validarDuplicidadeEmailCpf(String email, String cpf){
+        validarEmail(email);
+        validarCpf(cpf);
+    }
+
+    @Override
+    public void validarEmail(String email) {
+        /*ver se o email existe*/
+        boolean existeUsuarioComEsseEmail = usuarioRepository.existsByEmail(email);
+        if (existeUsuarioComEsseEmail) {
+            throw new RegraDeNegocioException("Já existe um usuário com esse email");
+        }
+    }
+
+    @Override
+    public void validarCpf(String cpf) {
+        /*ver se o cpf existe*/
+        boolean existeUsuarioComCpf = usuarioRepository.existsByCpf(cpf);
+
+        if (existeUsuarioComCpf) {
+            throw new RegraDeNegocioException("Já existe um usuário com esse CPF");
+        }
+    }
+
     /*public void senhaCriptografada(Usuario usuario) {
     	String pegarSenha = usuario.getSenha();
     	String criptografar = encoder.encode(pegarSenha);
     	usuario.setSenha(criptografar);
     }*/
-	
-	public void validacao(String email, String cpf) {
-		validarEmail(email);
-		validarCpf(cpf);
-	}	
-    
-    @Override
-    public void validarEmail(String email) {
-        /*ver se o email existe*/
-        boolean existeUsuarioComEsseEmail = usuarioRepository.existsByEmail(email);       
-        if (existeUsuarioComEsseEmail) {
-            throw new RegraDeNegocioException("Já existe um usuário com esse email");
-        }  
-    }
-	
-	@Override
-	public void validarCpf(String cpf) {
-		/*ver se existe o cpf*/
-		boolean existeUsuarioComEsseCpf = usuarioRepository.existsByCpf(cpf);
-		if (existeUsuarioComEsseCpf) {
-            throw new RegraDeNegocioException("Já existe um usuário com esse CPF");
-        }		
-	}   
+
 }
