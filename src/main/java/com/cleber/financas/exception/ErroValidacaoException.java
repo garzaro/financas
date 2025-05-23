22773bba82f554035b6e1088e7710fd8e7735db4
garzaro <clebergarzaro74@gmail.com>
2025-05-23 16:10:21 -0300

    inserindo classe de erro de validação

---
+++ src/main/java/com/cleber/financas/exception/ErroValidacaoException.java
@@ -0,0 +1,8 @@
+package com.cleber.financas.exception;
+
+public class ErroValidacaoException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	public ErroValidacaoException(String mensagem) {
+		super(mensagem);		
+	}	
+}
