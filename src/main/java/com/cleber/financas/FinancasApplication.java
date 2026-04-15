79f0a0945bf40e5894317b74c27f56c37945577c
garzaro <clebergarzaro74@gmail.com>
2026-04-14 22:40:29 -0300

    comentando o argon2

--- src/main/java/com/cleber/financas/FinancasApplication.java
+++ src/main/java/com/cleber/financas/FinancasApplication.java
@@ -1,14 +1,8 @@
 package com.cleber.financas;
 
-import com.cleber.financas.service.SenhaService;
-import com.cleber.financas.service.impl.SenhaServiceImpl;
-import com.password4j.Hash;
-import com.password4j.Password;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /*@EnableWebMvc - para liberar o acesso - configuração de CORS
 * allowedOrigins - (url) - urls permitidas para acessar um site
@@ -20,12 +14,12 @@ public class FinancasApplication implements WebMvcConfigurer {
 
 		SpringApplication.run(FinancasApplication.class, args);
 
-		SenhaService senhaService = new SenhaServiceImpl(new Argon2PasswordEncoder(16,
-				32,
-				2,
-				65536,
-				5));
-			String hash = senhaService.hashSenha("senha123");
-			System.out.println("ESSE É O HASH GERADO (classe de inicialização): " + hash);
+//		SenhaService senhaService = new SenhaServiceImpl(new Argon2PasswordEncoder(16,
+//				32,
+//				2,
+//				65536,
+//				5));
+//			String hash = senhaService.hashSenha("senha123");
+//			System.out.println("ESSE É O HASH GERADO (classe de inicialização): " + hash);
 		}
 	}
